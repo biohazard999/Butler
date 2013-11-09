0de4f7edef53e35ae595ef5b393bbc40f1645b30
m.grundner <m.grundner@delegate.at>
2013-11-09 01:28:43 +0100

    Tests for TheButler.Win are now correctly bootstrapped

---
+++ src/Application/TheDelegate.TheButler.Win.Tests/ProgramTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TheDelegate.TheButler.Win.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Program_CanBeLoaded_FromTestLocation()
+        {
+            Action action = () =>
+            {
+                var type = typeof (Program);
+                
+            };
+
+            action.ShouldNotThrow();
+        }
+    }
+}
--- src/Application/TheDelegate.TheButler.Win/Program.cs
+++ src/Application/TheDelegate.TheButler.Win/Program.cs
@@ -5,7 +5,7 @@ using System.Windows.Forms;
 
 namespace TheDelegate.TheButler.Win
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
